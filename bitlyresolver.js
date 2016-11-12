0f33983627f52be93445c7fd9a2e5fcd0250967a
Nir Moav <nirm@wix.com>
2016-11-13 00:26:11 +0200

    Basic (and rather ugly) implementation.

---
+++ bitlyresolver.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var statusDiv = null;
+var lastMove = 0;
+
+var END_POINT = 'https://bitlyresolver.herokuapp.com/resolveBitly?url=';
+
+var nodeNamesToIgnore = {
+  'html': 'html',
+  'body': 'body',
+  'video': 'video',
+  'audio': 'audio',
+  'object': 'object',
+  'script': 'script',
+  'form': 'form',
+  'iframe': 'iframe'
+};
+
+var lastBitlyLinks = {};
+var lastAjax = 0;
+
+function init() {
+  statusDiv = createStatusMessage();
+  document.body.appendChild(statusDiv);
+
+  document.addEventListener('mousemove', function(mouseEvent) {
+    if (Date.now() - lastAjax > 100 && mouseEvent && mouseEvent.target && mouseEvent.target.children.length === 0) {
+      var content = mouseEvent.target.innerText;
+      var linkToCheck = getLinkToCheck(content);
+      if (shouldCheckNode(mouseEvent.target) && linkToCheck) {
+        setBubblePosition(mouseEvent.clientX, mouseEvent.clientY);
+        if (lastBitlyLinks[content]) {
+          updateStatus(lastBitlyLinks[content]);
+        } else {
+          lastAjax = Date.now();
+          $.get(END_POINT + linkToCheck).then(function(res) {
+            lastBitlyLinks[content] = res;
+            updateStatus(lastBitlyLinks[content]);
+          });
+        }
+      } else {
+        hideStatus();
+      }
+    }
+  });
+};
+
+function getLinkToCheck(content) {
+  var index = content.indexOf('http://bit.ly');
+  if (index < 0) {
+    index = content.indexOf('https://bit.ly')
+  }
+  if (index < 0) {
+    index = content.indexOf('bit.ly');
+  }
+  if (index < 0) {
+    return null;
+  }
+  return content.substr(index);
+}
+
+function shouldCheckNode(node) {
+  var nodeName = (node || {}).nodeName;
+  var nameAsLowerCase = (nodeName || '').toLowerCase();
+  return !nodeNamesToIgnore[nameAsLowerCase];
+}
+
+function setBubblePosition(x, y) {
+  if (y + 20 > document.documentElement.clientHeight) {
+    y -= 20;
+  }
+  if (x + 150 > document.documentElement.clientWidth) {
+    x-= 200;
+  }
+  statusDiv.style.top = y + 'px';
+  statusDiv.style.left = x + 'px';
+}
+
+function updateStatus(result) {
+  if (result.status === '200') {
+    statusDiv.innerHTML = result.resolvedUrl;
+    statusDiv.style.opacity = 1;
+    var statusDivPos = $(statusDiv).position();
+    var statusDivWidth = $(statusDiv).width();
+    if (statusDivPos.left + statusDivWidth > document.documentElement.clientWidth) {
+      statusDiv.style.right = 0;
+      statusDiv.style.left = '';
+    } else {
+      statusDiv.style.right = '';
+    }
+  }
+}
+
+function hideStatus() {
+  statusDiv.style.opacity = 0;
+}
+
+function createStatusMessage() {
+  var statusDiv = document.createElement('div');
+  statusDiv.innerHTML = '';
+  var statusStyle = statusDiv.style;
+  statusStyle.background = 'white';
+  statusStyle.border = '1px solid black';
+  statusStyle.padding = '5px';
+  statusStyle.position = 'fixed';
+  statusStyle.opacity = 0;
+  statusStyle.transition = ".3s ease all";
+  statusStyle["box-shadow"] = '5px 5px 5px #8ec78e';
+  statusStyle["max-width"] = '100%';
+  statusStyle["z-index"] = 999999;
+  statusStyle["pointer-events"] = "none";
+
+  return statusDiv;
+}
+
+init();
